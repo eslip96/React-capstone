b668571534a28d84e153f56d35b7f458d7e303da
Enoka <eslip96@gmail.com>
2023-04-28 23:41:00 -0600

    added to about

--- src/components/AboutView.js
+++ src/components/AboutView.js
@@ -8,25 +8,18 @@ const AboutView = () => {
         <div className="row">
           <div className="col-lg-8 offset-lg-2 my-5">
             <p className="lead">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis a
-              rerum impedit quo maiores tenetur placeat, hic modi consequuntur
-              perferendis incidunt quisquam quaerat possimus laborum ratione
-              sapiente odit! Voluptatem, nam? Sed, dolorum a eos corporis
-              nesciunt facere ratione? Voluptas fugit debitis ipsum quasi
-              repellendus alias doloribus iusto, rem nisi consequatur
-              perspiciatis ab saepe provident ducimus temporibus, tempora atque
-              explicabo quas. Suscipit obcaecati commodi alias consequatur
-              laborum nihil natus deleniti ipsam? Similique quisquam,
-              consequuntur quasi hic mollitia perferendis labore necessitatibus
-              tempora debitis nisi, numquam minus voluptatibus odio accusantium
-              ullam aliquam ab? Nam vero quas iusto nisi voluptatibus
-              accusantium dolor sunt possimus, placeat perferendis expedita quo
-              modi, officia asperiores recusandae velit totam aliquam pariatur
-              repellat dolores aut dicta distinctio corrupti tempora. Impedit.
-              Sequi earum aut consequatur illum iste soluta cupiditate ut?
-              Dolorum, rem unde! Nesciunt aperiam voluptatibus quas cum quos
-              dolore, quo quam excepturi quisquam. Reiciendis dolor ratione
-              nostrum, consequatur id reprehenderit?
+              My ultimate goal is to become a proficient developer and secure a
+              job in the music industry where I can make a valuable
+              contribution. During my time at DevPipeline, I've been humbled by
+              the complexity of coding, which I used to think was beyond my
+              grasp. However, I've come a long way since I first started a year
+              ago and feel like I've learned a lot. I've built several projects
+              throughout my journey, but my favorite language so far has to be
+              React. I love how its component-based structure allows for easy
+              reusability, and how it ties together all the front-end
+              development skills I've acquired so far. Anytime I can use
+              existing components and avoid writing new code is a huge plus for
+              me!
             </p>
           </div>
         </div>
